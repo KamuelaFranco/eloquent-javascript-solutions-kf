0768eae877e3818bf261087fdd248dd21b86dd6e
Kamuela Franco <kamuela.franco@gmail.com>
2015-08-14 12:41:54 +0100

    Fixed Chapter 2

--- ch01/fizz-buzz.js
+++
@@ -1,7 +0,0 @@
-for (var num=1;num<=100;num++) {
-	if (num%3==0) {
-		if (num%5==0) console.log("FizzBuzz");
-		else console.log("Fizz");
-	} else if (num%5==0) console.log("Buzz");
-	else console.log(num);
-}
--- ch01/looping-a-triangle.js
+++
@@ -1,7 +0,0 @@
-for (var lineNo = 1; lineNo <= 7; lineNo++) {
-	var row = "";
-	for (var hashCount = 1; hashCount <= lineNo; hashCount++) {
-		row += "#";
-	}
-	console.log(row);
-}
